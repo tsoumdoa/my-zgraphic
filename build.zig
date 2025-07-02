319f2656c7af2de3853c4a9bc761526365074db9
tsoumdoa <8blondes.swerves@icloud.com>
2025-07-02 22:49:32 +0800

    triangle runs for now

--- build.zig
+++ build.zig
@@ -1,47 +1,84 @@
 const std = @import("std");
+const ResolvedTarget = std.Build.ResolvedTarget;
+const Step = std.Build.Step;
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    for ([_]struct { name: []const u8 }{
+        .{ .name = "wgpu-triangle" },
+    }) |example| {
+        const exe_mod = b.createModule(.{
+            .root_source_file = b.path(b.fmt("src/{s}/main.zig", .{example.name})),
+            .target = target,
+            .optimize = optimize,
+        });
 
-    const exe = b.addExecutable(.{
-        .name = "my_zgraphic",
-        .root_module = exe_mod,
-    });
+        const exe = b.addExecutable(.{
+            .name = example.name,
+            .root_module = exe_mod,
+        });
 
+        addDeps(exe, b, target);
+
+        const run_cmd = b.addRunArtifact(exe);
+
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step(b.fmt("run-{s}", .{example.name}), b.fmt("Run {s}", .{example.name}));
+        run_step.dependOn(&run_cmd.step);
+    }
+}
+
+inline fn addDeps(exe: *Step.Compile, b: *std.Build, target: ResolvedTarget) void {
+
+    //add zglfw
     const zglfw = b.dependency("zglfw", .{});
     exe.root_module.addImport("zglfw", zglfw.module("root"));
 
+    // add zgpu
+    @import("zgpu").addLibraryPathsTo(exe);
+    const zgpu = b.dependency("zgpu", .{});
+    exe.root_module.addImport("zgpu", zgpu.module("root"));
+
+    // add zpool
+    const zpool = b.dependency("zpool", .{});
+    exe.root_module.addImport("zpool", zpool.module("root"));
+
+    // add zgui
+    const zgui = b.dependency("zgui", .{
+        .shared = false,
+        .with_implot = true,
+        .backend = .glfw_wgpu,
+    });
+    exe.root_module.addImport("zgui", zgui.module("root"));
+
+    // add zmath
+    const zmath = b.dependency("zmath", .{});
+    exe.root_module.addImport("zmath", zmath.module("root"));
+
+    // custom utils + helper functions
+    const funcsModule = b.addModule("funcs", .{
+        .root_source_file = .{ .src_path = .{
+            .owner = b,
+            .sub_path = "src/funcs//utils.zig",
+        } },
+    });
+    funcsModule.addImport("zgpu", zgpu.module("root"));
+    exe.root_module.addImport("utils", funcsModule);
+
+    // link libs
     if (target.result.os.tag != .emscripten) {
         exe.linkLibrary(zglfw.artifact("glfw"));
+        exe.linkLibrary(zgpu.artifact("zdawn"));
+        exe.linkLibrary(zgui.artifact("imgui"));
     }
-
     b.installArtifact(exe);
-    
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    b.installFile("public/Roboto-Medium.ttf", "bin/resource/Roboto-Medium.ttf");
 }
---
+++ src/funcs/utils.zig
@@ -0,0 +1,21 @@
+const zgpu = @import("zgpu");
+
+pub inline fn createDepthTexture(gctx: *zgpu.GraphicsContext) struct {
+    texture: zgpu.TextureHandle,
+    view: zgpu.TextureViewHandle,
+} {
+    const texture = gctx.createTexture(.{
+        .usage = .{ .render_attachment = true },
+        .dimension = .tdim_2d,
+        .size = .{
+            .width = gctx.swapchain_descriptor.width,
+            .height = gctx.swapchain_descriptor.height,
+            .depth_or_array_layers = 1,
+        },
+        .format = .depth32_float,
+        .mip_level_count = 1,
+        .sample_count = 1,
+    });
+    const view = gctx.createTextureView(texture, .{});
+    return .{ .texture = texture, .view = view };
+}
